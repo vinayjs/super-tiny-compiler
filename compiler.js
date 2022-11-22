aeb2854a142817e9911a2ec998882d695d1fefb4
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-22 10:54:13 +0530

    add code genrator func to work on build AST

--- compiler.js
+++ compiler.js
@@ -190,6 +190,43 @@ const transformer = (ast) => {
   return newAst;
 };
 
+// code generator will print, all of the different nodes  & will recursively call itself to print nested
+// nodes until everything is printed into one long string of code.
+
+const codeGenerator = (node) => {
+  switch (node.type) {
+
+    case 'Program':
+      return node.body.map(codeGenerator)
+        .join('\n');
+
+    case 'ExpressionStatement':
+      return (
+        codeGenerator(node.expression) + ';' 
+      );
+    case 'CallExpression':
+      return (
+        codeGenerator(node.callee) +
+        '(' +
+        node.arguments.map(codeGenerator)
+          .join(', ') +
+        ')'
+      );
+
+    case 'Identifier':
+      return node.name;
+
+    case 'NumberLiteral':
+      return node.value;
+
+    case 'StringLiteral':
+      return '"' + node.value + '"';
+
+    default:
+      throw new TypeError(node.type);
+  }
+}
+
 // let tokens = [
 //   { type: "paren", value: "(" },
 //   { type: "string", value: "add" },
